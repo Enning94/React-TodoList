6de004cdfe424eafb27bf4ad6f6f36a0ff2362f1
Enning94 <enningfestus@gmail.com>
2022-12-06 00:56:41 +0000

    Add styling

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,22 +1,24 @@
+import { v4 as uuidv4 } from "uuid";
 import React from "react";
 import TodosList from "./TodosList";
 import Header from "./Header";
+import InputTodo from "./InputTodo";
 
 class TodoContainer extends React.Component {
   state = {
     todos: [
       {
-        id: 1,
+        id:  uuidv4(),
         title: "Setup development environment",
         completed: true,
       },
       {
-        id: 2,
+        id:  uuidv4(),
         title: "Develop website and add content",
         completed: false,
       },
       {
-        id: 3,
+        id:  uuidv4(),
         title: "Deploy to live server",
         completed: false,
       },
@@ -47,15 +49,23 @@ class TodoContainer extends React.Component {
     });
   };
 
+  addTodoItem = (title) => {
+    const newTodo = { id:  uuidv4(), title: title, completed: false };
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
+
   render() {
     return (
-      <div>
+        <div className="container">
+      <div className="inner">
         <Header />
+        <InputTodo addTodoProps={this.addTodoItem} />
         <TodosList
           todos={this.state.todos}
           handleChangeProps={this.handleChange}
           deleteTodoProps={this.delTodo}
         />
+      </div>
       </div>
     );
   }
