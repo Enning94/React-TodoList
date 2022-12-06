3de8d98303f0b0d203d1b918c94b64aa5f4b9fc2
Enning94 <enningfestus@gmail.com>
2022-12-06 01:14:04 +0000

    Add edit todo item functionality

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -8,17 +8,17 @@ class TodoContainer extends React.Component {
   state = {
     todos: [
       {
-        id:  uuidv4(),
+        id: uuidv4(),
         title: "Setup development environment",
         completed: true,
       },
       {
-        id:  uuidv4(),
+        id: uuidv4(),
         title: "Develop website and add content",
         completed: false,
       },
       {
-        id:  uuidv4(),
+        id: uuidv4(),
         title: "Deploy to live server",
         completed: false,
       },
@@ -50,22 +50,33 @@ class TodoContainer extends React.Component {
   };
 
   addTodoItem = (title) => {
-    const newTodo = { id:  uuidv4(), title: title, completed: false };
+    const newTodo = { id: uuidv4(), title: title, completed: false };
     this.setState({ todos: [...this.state.todos, newTodo] });
   };
 
+  setUpdate = (updatedTitle, id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.title = updatedTitle
+        }
+        return todo
+      }),
+    })
+  }
   render() {
     return (
-        <div className="container">
-      <div className="inner">
-        <Header />
-        <InputTodo addTodoProps={this.addTodoItem} />
-        <TodosList
-          todos={this.state.todos}
-          handleChangeProps={this.handleChange}
-          deleteTodoProps={this.delTodo}
-        />
-      </div>
+      <div className="container">
+        <div className="inner">
+          <Header />
+          <InputTodo addTodoProps={this.addTodoItem} />
+          <TodosList
+            todos={this.state.todos}
+            handleChangeProps={this.handleChange}
+            deleteTodoProps={this.delTodo}
+            setUpdate={this.setUpdate}
+          />
+        </div>
       </div>
     );
   }
